e8cfc151f582df81ee43e787e946ad65fa45b53f
Ahmed Saad <ahmed430.saad@gmail.com>
2024-12-19 03:44:03 +0200

    Adding elements to combo boxes

--- Project/src/main/java/com/marwan/weather/monitoring/system/SecondaryController.java
+++ Project/src/main/java/com/marwan/weather/monitoring/system/SecondaryController.java
@@ -1,12 +1,39 @@
 package com.marwan.weather.monitoring.system;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 
-public class SecondaryController {
+public class SecondaryController implements Initializable {
+     @FXML
+    private ComboBox<String> tempList;
+
+    @FXML
+    private ComboBox<String> windList;
+
+    @FXML
+    void setTemp(ActionEvent event) {
+
+    }
+
 
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
     }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+      String [] TemperatureMeasurmentUnit = {"°C" , "°F"}; 
+      String [] WindSpeedMeasurmentUnit = {"Km\\h" , "M\\h"};
+      tempList.getItems().addAll(TemperatureMeasurmentUnit);
+      windList.getItems().addAll(WindSpeedMeasurmentUnit);
+
+    }
+
 }
